b88f7643b9eccacb1a6d0191d21d959cbc27895d
Fatema <fatematz486@gmail.com>
2026-04-15 00:13:54 +0600

    add loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+      <div className="flex justify-center items-center p-20 ">
+            <span className="loading loading-ring loading-xl"></span>
+        </div>
+    );
+};
+
+export default loading;
